dce32f4886740f12e3dedd039d8b33dc3f3fd603
rakeshgzp <rprakeshprasad00@gmail.com>
2020-06-17 23:16:29 +0530

    Recipes

---
+++ angular/module2/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is simply a url', 'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg'),
+    new Recipe('A test Recipe', 'This is simply a url', 'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg'),
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
